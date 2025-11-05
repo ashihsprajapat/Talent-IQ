6b5b858791c06260791568a647f76fd973712818
ashish prajapat <ashishprajapat507@gmail.com>
2025-11-05 12:38:33 +0530

    ok

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -1,17 +1,34 @@
 
 
 import express from 'express'
+import path from 'path'
 import { ENV } from './lib/env.js';
 
 const app = express();
 
+const __dirname = path.resolve()
 
 app.listen(ENV.PORT, () => {
     console.log("Server is running on port", ENV.PORT);
 })
 
 
-app.get("/", (req, res) => {
-
-    res.send("Api is working successfully")
+app.get("/test", (req, res) => {
+    res.json({ message: "Api is working successfully" })
 })
+
+app.get("/books", (req, res) => {
+    res.json({ message: "this is the book endpoint" })
+})
+
+
+//make our app ready for deployment
+
+if (ENV.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("/{*any}", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+
+}
